b3cca9d8593c44b26c7d2b3e8e20ddbd3f119f35
ddelfaus <ddelfaus@gmail.com>
2019-12-13 11:33:23 -0500

    basic db creted

---
+++ data/migrations/20191213111430_project1.js
@@ -0,0 +1,77 @@
+
+exports.up = function(knex) {
+  return knex.schema
+
+  .createTable("projects" ,tbl => {
+      tbl.increments();
+
+      tbl.string("name", 255)
+        .notNullable()
+        .unique();
+
+      tbl.string("description", 255)
+    
+      tbl.bool('completed')
+  })
+
+  .createTable("resources", tbl => {
+      tbl.increments();
+
+      tbl.string("name", 255)
+        .notNullable()
+        .unique();
+    
+    tbl.string("description", 255)
+  })
+
+  .createTable("project-resource", tbl => {
+      tbl.increments();
+
+      tbl.integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+
+        
+      tbl.integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("resources")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+
+  })
+
+  .createTable("Tasks", tbl => {
+      tbl.increments();
+      tbl.string("description", 255)
+        .notNullable()
+    
+      tbl.string("notes", 255)
+
+      tbl.bool('completed')
+
+              
+      tbl.integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+
+  })
+
+};
+
+exports.down = function(knex) {
+    return knex.schema
+    .dropTableIfExists('projects')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('project-resource')
+    .dropTableIfExists('Tasks')
+};
